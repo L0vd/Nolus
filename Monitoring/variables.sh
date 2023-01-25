a9a2f4ac80cd51385154654601b2790a229e9bf3
NktSH <49846495+NktSH@users.noreply.github.com>
2023-01-25 21:15:26 +0300

    Add files via upload

---
+++ Monitoring/variables.sh
@@ -0,0 +1,5 @@
+#Nolus monitoring variables template 
+COS_BIN_NAME=insert_path_to_nolus_binary     # example: /root/go/bin/nolusd or /home/user/go/bin/nolusd
+COS_PORT_RPC=insert_port_here                # default: 26657
+COS_VALOPER=insert_valoper_here              # example: nolusvaloper1234545636767376535673
+LOGENTRY=nolus
